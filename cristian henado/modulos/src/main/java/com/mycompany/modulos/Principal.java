3b8635529d021f42c569566b26d3fd0329e817a1
Sena <dimatesebas73@gmail.com>
2024-09-05 16:11:20 -0500

    actualizacion

---
+++ cristian henado/modulos/src/main/java/com/mycompany/modulos/Principal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.modulos;
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
